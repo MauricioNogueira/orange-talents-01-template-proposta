f654d85f22068b00ddf4994c197b65b690d8b23c
MauricioNogueira <mauricio.nogueiraa@gmail.com>
2021-03-02 15:00:26 -0300

    Correção no application-prod.properties

--- src/main/java/br/com/zup/proposta/config/WebSecurity.java
+++ src/main/java/br/com/zup/proposta/config/WebSecurity.java
@@ -1,6 +1,5 @@
 package br.com.zup.proposta.config;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
